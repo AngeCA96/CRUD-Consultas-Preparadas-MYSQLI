5ac6941e53fab42c03d0923514f90f17bfa64097
V. Ángeles Che Aguilar <virginiacheagu@gmail.com>
2022-04-21 13:54:25 -0500

    Create pie.php

---
+++ pie.php
@@ -0,0 +1,14 @@
+        </main>
+
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+
+        <footer class="px-2 py-2 fixed-bottom bg-dark">
+            <span class="text-muted">CRUD con MYSQLI creado por 
+                <a class="text-white" href="https://github.com/AngeCA96">AngeCA96</a>
+            </span>
+        </footer>
+    </body>
+</html>
